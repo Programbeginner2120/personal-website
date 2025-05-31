0b66a958d31eb51b7ec7434140bae68048fd7928
Matthew Killeen <Matthewkilleen99@gmail.com>
2025-05-31 12:43:03 -0400

    fleshed out the main section component and added a background linear gradient to the whole app

--- src/app/components/landing-page/landing-page.component.ts
+++ src/app/components/landing-page/landing-page.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { MainSectionComponent } from '../main-section/main-section.component';
 
 @Component({
   selector: 'app-landing-page',
   standalone: true,
-  imports: [],
+  imports: [MainSectionComponent],
   templateUrl: './landing-page.component.html',
   styleUrl: './landing-page.component.scss'
 })
