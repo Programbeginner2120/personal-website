3b48d53d4498142ee38dd1224049c3490be86b37
Matthew Killeen <Matthewkilleen99@gmail.com>
2025-06-01 15:24:58 -0400

    got animation set up for networking section component

--- src/app/components/landing-page/landing-page.component.ts
+++ src/app/components/landing-page/landing-page.component.ts
@@ -3,10 +3,11 @@ import { MainSectionComponent } from '../main-section/main-section.component';
 import { AboutMeSectionComponent } from '../about-me-section/about-me-section.component';
 import { ExperienceSectionComponent } from '../experience-section/experience-section.component';
 import { FeaturedProjectsSectionComponent } from '../featured-projects-section/featured-projects-section.component';
+import { NetworkingSectionComponent } from '../networking-section/networking-section.component';
 
 @Component({
     selector: 'app-landing-page',
-    imports: [MainSectionComponent, AboutMeSectionComponent, ExperienceSectionComponent, FeaturedProjectsSectionComponent],
+    imports: [MainSectionComponent, AboutMeSectionComponent, ExperienceSectionComponent, FeaturedProjectsSectionComponent, NetworkingSectionComponent],
     templateUrl: './landing-page.component.html',
     styleUrl: './landing-page.component.scss'
 })
--- src/app/components/networking-section/networking-section.component.ts
+++ src/app/components/networking-section/networking-section.component.ts
@@ -1,11 +1,43 @@
-import { Component } from '@angular/core';
+import { animate, state, style, transition, trigger } from '@angular/animations';
+import { afterNextRender, Component, ElementRef, inject, signal, Signal } from '@angular/core';
+import { ScrollAnimationService } from '../../services/shared/scroll-animation/scroll-animation.service';
+import { DataService } from '../../services/shared/data/data.service';
+import { toSignal } from '@angular/core/rxjs-interop';
 
 @Component({
     selector: 'app-networking-section',
     imports: [],
     templateUrl: './networking-section.component.html',
-    styleUrl: './networking-section.component.scss'
+    styleUrl: './networking-section.component.scss',
+    animations: [
+    trigger('fadeInUp', [
+      state('hidden', style({ opacity: 0, transform: 'translateY(30px)' })),
+      state('visible', style({ opacity: 1, transform: 'translateY(0)' })),
+      transition('hidden => visible', animate('500ms ease-out'))
+    ])
+  ]
 })
 export class NetworkingSectionComponent {
+
+    //DI
+    private scrollService = inject(ScrollAnimationService);
+    private elementRef = inject(ElementRef);
+    private dataService = inject(DataService);
+
+    // Signals
+    isVisible: Signal<boolean> = signal(false);
+
+    networkingSectionData = toSignal(this.dataService.getData('/assets/website-data/networking-section.json'));
+
+    constructor() {
+        afterNextRender(() => {
+            const visibilitySignal = this.scrollService.observeElement(this.elementRef);
+            this.isVisible = visibilitySignal;
+        });
+    }
+
+    ngOnDestroy() {
+        this.scrollService.unobserveElement(this.elementRef);
+    }
 
 }
