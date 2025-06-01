e1c1c08192b373c74e4bea3f694a5cb3182a3f6f
Matthew Killeen <Matthewkilleen99@gmail.com>
2025-06-01 11:51:25 -0400

    got reading of json working for main section component

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -4,6 +4,7 @@ import { provideRouter, withInMemoryScrolling } from '@angular/router';
 import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
 import { provideAnimations } from '@angular/platform-browser/animations';
+import { provideHttpClient } from '@angular/common/http';
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -16,5 +17,6 @@ export const appConfig: ApplicationConfig = {
     })), 
     provideClientHydration(),
     provideAnimations(),
+    provideHttpClient()
   ]
 };
--- src/app/components/main-section/main-section.component.ts
+++ src/app/components/main-section/main-section.component.ts
@@ -1,10 +1,12 @@
-import { Component, inject } from '@angular/core';
+import { Component, computed, inject, Signal } from '@angular/core';
 import { SocialsButton } from '../../interfaces/buttons.interface';
 import { SocialsButtonComponent } from '../shared/socials-button/socials-button.component';
 import { AppButtonComponent } from '../shared/app-button/app-button.component';
 import { FileDownloadService } from '../../services/shared/file-download/file-download.service';
 import { AppDownloadButtonComponent } from '../shared/app-download-button/app-download-button.component';
 import { FileDownloadRequest } from '../../interfaces/file.interface';
+import { DataService } from '../../services/shared/data/data.service';
+import { toSignal } from '@angular/core/rxjs-interop';
 
 @Component({
     selector: 'app-main-section',
@@ -14,25 +16,45 @@ import { FileDownloadRequest } from '../../interfaces/file.interface';
 })
 export class MainSectionComponent {
 
-    readonly fileDownloadService = inject(FileDownloadService);
+    // DI
+    private readonly fileDownloadService = inject(FileDownloadService);
+    private readonly dataService = inject(DataService);
 
-    readonly socialsButtons: SocialsButton[] = [
-        {
-            href: 'https://github.com/Programbeginner2120',
-            iconClasses: ["fab", "fa-github"],
-            isEmail: false
-        },
-        {
-            href: 'https://www.linkedin.com/in/matthew-r-killeen',
-            iconClasses: ["fab", "fa-linkedin"],
-            isEmail: false
-        },
-        {
-            href: 'mailto:Matthewkilleen99@gmail.com',
-            iconClasses: ["fa-solid", "fa-envelope"],
-            isEmail: false
+    mainSectionData = toSignal(this.dataService.getData('/assets/website-data/main-section.json'));
+
+    readonly name = computed(() => {
+        if (!this.mainSectionData()) {
+            return;
         }
-    ];
+
+        return this.mainSectionData()['name'];
+    });
+
+    readonly description = computed(() => {
+        if (!this.mainSectionData()) {
+            return;
+        }
+
+        return this.mainSectionData()['description'];
+    });
+
+    readonly socialsButtons: Signal<SocialsButton[] | undefined> = computed(() => {
+        if (!this.mainSectionData()) {
+            return;
+        }
+
+        const mainSectionData = this.mainSectionData();
+        const socialsButtons: any[] = mainSectionData['socialsButtons'];
+
+        return socialsButtons.map(socialsButton => {
+            return {
+                href: socialsButton['href'],
+                iconClasses: socialsButton['iconClasses'],
+                label: socialsButton['label'],
+                isEmail: socialsButton['isEmail']
+            } as SocialsButton
+        });
+    });
 
     handleButtonClick() {
         console.log("BUTTON CLICKED!");
