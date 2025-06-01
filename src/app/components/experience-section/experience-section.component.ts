c41e0f186f10d8386768c263878e51f907efd109
Matthew Killeen <Matthewkilleen99@gmail.com>
2025-06-01 09:40:59 -0400

    put a gap between landing page flex elements and introduced animation for experience section

--- src/app/components/experience-section/experience-section.component.ts
+++ src/app/components/experience-section/experience-section.component.ts
@@ -1,11 +1,38 @@
-import { Component } from '@angular/core';
+import { animate, state, style, transition, trigger } from '@angular/animations';
+import { afterNextRender, Component, ElementRef, inject, signal, Signal } from '@angular/core';
+import { ScrollAnimationService } from '../../services/shared/scroll-animation/scroll-animation.service';
 
 @Component({
     selector: 'app-experience-section',
     imports: [],
     templateUrl: './experience-section.component.html',
-    styleUrl: './experience-section.component.scss'
+    styleUrl: './experience-section.component.scss',
+    animations: [
+    trigger('fadeInUp', [
+      state('hidden', style({ opacity: 0, transform: 'translateY(30px)' })),
+      state('visible', style({ opacity: 1, transform: 'translateY(0)' })),
+      transition('hidden => visible', animate('500ms ease-out'))
+    ])
+  ]
 })
 export class ExperienceSectionComponent {
+
+    //DI
+    private scrollService = inject(ScrollAnimationService);
+    private elementRef = inject(ElementRef);
+
+    // Signals
+    isVisible: Signal<boolean> = signal(false);
+
+    constructor() {
+        afterNextRender(() => {
+            const visibilitySignal = this.scrollService.observeElement(this.elementRef);
+            this.isVisible = visibilitySignal;
+        });
+    }
+
+    ngOnDestroy() {
+        this.scrollService.unobserveElement(this.elementRef);
+    }
 
 }
--- src/app/components/landing-page/landing-page.component.ts
+++ src/app/components/landing-page/landing-page.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { MainSectionComponent } from '../main-section/main-section.component';
 import { AboutMeSectionComponent } from '../about-me-section/about-me-section.component';
+import { ExperienceSectionComponent } from '../experience-section/experience-section.component';
 
 @Component({
     selector: 'app-landing-page',
-    imports: [MainSectionComponent, AboutMeSectionComponent],
+    imports: [MainSectionComponent, AboutMeSectionComponent, ExperienceSectionComponent],
     templateUrl: './landing-page.component.html',
     styleUrl: './landing-page.component.scss'
 })
